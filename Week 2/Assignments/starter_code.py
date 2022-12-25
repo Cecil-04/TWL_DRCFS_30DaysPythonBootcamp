06b1410b2d81454c3af1686af4a78956da166b9c
Cecil-04 <bibekmagar2342@gmail.com>
2022-12-25 16:45:20 +0545

    change

--- Week 2/Assignments/starter_code.py
+++ Week 2/Assignments/starter_code.py
@@ -2,11 +2,12 @@
 # Please donot write code outside this boundry or you may fail the test.
 
 # start by importing the necessary documents
-
+import os
 import random
 import string 
+dirname = os.path.dirname(__file__)
 
-def check_password_strength(pwd: str) -> str:
+def rank(pwd: str) -> str:
     '''
     Ranker function that ranks the password based on the assigned criteria
 
@@ -33,43 +34,40 @@ def check_password_strength(pwd: str) -> str:
     Returns: rank -> rank of password; POOR / MODERATE / STRONG
     '''
     ## Start code here
-
-    lower_count = 0
-    upper_count = 0
-    digit_count = 0
-    symbol_count = 0
-
-    for c in pwd:
-        if c.islower():
-            lower_count += 1
-        elif c.isupper():
-            upper_count += 1
-        elif c.isdigit():
-            digit_count +=1
-        elif c in string.punctuation:
-            symbol_count += 1
-
+    def has_lowercase(pwd) -> bool:
+        return True if any(char.islower() for char in pwd) else False
     
-    if lower_count <1 or upper_count <1 or digit_count <1 or symbol_count <1:
-        strength = 'POOR'
-    elif len(pwd) < 8 or len(pwd) > 10:
-        strength = 'POOR'
-    elif len(pwd) < 8 or len(pwd) > 10:
-        strength = 'POOR'
-    elif len(pwd) >= 8 and len(pwd) <= 10:
-        strength = 'MODERATE'
+
+    def has_uppercase(pwd) -> bool:
+        return True if any(char.isupper() for char in pwd) else False
+
+    def has_chars(pwd) -> bool:
+        symbols = '! + _ = ? # % * @ & _ ^ $'. split('')
+        return True if any(char in symbols for char in pwd) else False
+
+    def has_nums(pwd) -> bool:
+        return True if any(char.isdigit() for char in pwd) else False
+
+
+    if has_lowercase(pwd) and has_uppercase(pwd) and has_chars(pwd) and has_nums(pwd) and len(pwd) > 10:
+        return 'STRONG'
+    if (has_chars(pwd) + has_lowercase(pwd) + has_nums(pwd) + has_uppercase(pwd)) >= 3 and len(pwd) >= 8:
+        return "MODERATE"
     else:
-        strength = 'STRONG'
-    ## End code here
-    return strength
+        return "POOR"
+    return 'ERROR RATING PASSWORD'
 
-while True:
-        print('\nMenu:')
-        print('1. Check passwords in a file')
-        print('2. Generate a new password')
-        print('3. Quit')
+def write_file(checked_file , line: tuple):
+   
+    try:
+        checked_file.write(','.join(line))
+        checked_file.write('\n')
+    except IOError:
+        print('IOError occured, restarting the program...')
+        pass
 
-if choice == '1':
+
+
 
 def option1():
     '''
@@ -84,24 +82,101 @@ def option1():
     # 5. Close necessary files and print to terminal.
     
     ## START CODE HERE
-    if choice == '1':
-        with open(r'C:\Users\Hp\Desktop\TWL_DRCFS_30DaysPythonBootcamp\Week 2\Assignments\Users-Pwds(10).txt','r') as input_file:
-            with open(r'C:\Users\Hp\Desktop\TWL_DRCFS_30DaysPythonBootcamp\Week 2\Assignments\Users-Pwds(200).txt','w') as output_file:
-                for line in input_file:
-                    username, password = line.strip().split(',')
-                    strength = check_password_strength(password)
+    print('-'*80)
+    print('Please select one of 2 options')
+    print('option 1. rank passwords from Users-Pwds(200).txt \t option 2. rank passwords from Users-Pwds(10).txt \noption 3. Enter your own path')
 
-                    output_file.write(f'{username}, {password}, {strength}\n')
-        print(f'Processed {line_count} passwords. Output can be found in Users-Pwds-Chked.txt')
+    option = input('Enter your option: ')
+    option = check_int_type(option)
+
+
+    if option == 1:
+        checked = open(os.path.join(dirname,r'C:\Users\Hp\Desktop\TWL_DRCFS_30DaysPythonBootcamp\Week 2\Assignments\Users-Pwds(10).txt'),'w')
+        usrpwds = open(os.path.join(dirname,r'C:\Users\Hp\Desktop\TWL_DRCFS_30DaysPythonBootcamp\Week 2\Assignments\Users-Pwds(200).txt'),'r').read().split()
+
+        for userpwd in usrpwds:
+            username, password = userpwd.split(',')
+            strength = rank(password) # give rank to the current password
+
+            line = (username,password,strength)
+            write_file(checked, line) # use helper function to write the line in given file
+
+        checked.close()
+        print('#'*80)
+        print('[INFO] '+'Number of passwords checked:',str(len(usrpwds)))
+        print('[INFO] '+'The given rankings can be found in Users-Pwds-Chked.txt')
+        print('#'*80)
+
+
+    elif option == 2:
+            checked = open(os.path.join(dirname,r'C:\Users\Hp\Desktop\TWL_DRCFS_30DaysPythonBootcamp\Week 2\Assignments\Users-Pwds(10).txt'),'w')
+            usrpwds = open(os.path.join(dirname,r'C:\Users\Hp\Desktop\TWL_DRCFS_30DaysPythonBootcamp\Week 2\Assignments\Users-Pwds(200).txt'),'r').read().split()
+            for userpwd in usrpwds:
+                username, password = userpwd.split(',')
+                strength = rank(password)
+
+                line = (username,password,strength)
+                write_file(checked, line)
+        
+            checked.close()
+            print('#'*80)
+            print('[INFO] '+'Number of passwords checked:',str(len(usrpwds)))
+            print('[INFO] '+'The given rankings can be found in Users-Pwds-Chked.txt')
+            print('#'*80)
+
+    elif option == 3:
+            path = input("Enter your absolute (not relative) filepath: ")
+            if path[-3:].upper() == 'TXT':
+                try:
+                    usrpwds = open(path,'r').read().split()
+                    checked = open(os.path.join(dirname,'Users-Pwds-Chked.txt'),'w')
+                    for userpwd in usrpwds:
+                        username, password = userpwd.split(',')
+                        strength = rank(password)
+
+                        line = (username,password,strength)
+                        write_file(checked, line)
+                    checked.close()
+                    print('#'*80)
+                    print('[INFO] '+'Number of passwords checked:',str(len(usrpwds)))
+                    print('[INFO] '+'The given rankings can be found in Users-Pwds-Chked.txt')
+                    print('#'*80)
+                except FileNotFoundError:
+                    print('\n'+'*'*80)
+                    print("Sorry we couldnot find your file in the given path:")
+                    print(path + 'IS INVALID, restarting the program...')
+                    print('#'*80+ '\n')
+            else:
+                try:
+                    path = path+'.txt'
+                    usrpwds = open(path,'r').read().split()
+                    checked = open(os.path.join(dirname,'Users-Pwds-Chked.txt'),'w')
+                    for userpwd in usrpwds:
+                        username, password = userpwd.split(',')
+                        strength = rank(password)
+
+                        line = (username,password,strength)
+                        write_file(checked, line)
+                    checked.close()
+                    print('#'*80)
+                    print('[INFO] '+'Number of passwords checked:',str(len(usrpwds)))
+                    print('[INFO] '+'The given rankings can be found in Users-Pwds-Chked.txt')
+                    print('#'*80)
+                except FileNotFoundError:
+                    print('\n'+'#'*80)
+                    print("Sorry we couldnot find your file in the given path:")
+                    print(path + 'IS INVALID, restarting the program...')
+                    print('#'*80+ '\n')
+
+    else:
+            print('Please choose between 1 and 3')
+
+
+       
     ## END CODE HERE
 
-    print('#'*80)
-    # [INFO] Be sure to change userpwds with the name of variable that you give to the list of passwords
-    print('[INFO] '+'Number of passwords checked:',str(len(usrpwds))) 
-    print('[INFO] '+'The given rankings can be found in Users-Pwds-Chked.txt')
-    print('#'*80)
+    
 
-elif choice == '2':
     
 def option2():
     '''
@@ -117,7 +192,7 @@ def option2():
         Then the program loops back to the menu again offering displaying and offering to select 1, 2 or 3.
     '''
 
-    def generate() -> str:
+    def gen_pass() -> str:
         '''
         Helper function to generate password.
         Returns: A string pwd with strong ranking in our ranking system.
@@ -130,26 +205,88 @@ def option2():
         chars = string.punctuation
         digits = string.digits
         pwd = ''
+        while True:
+            pwd+=random.choice(Ualphabets)
+            pwd+=random.choice(Lalphabets)
+            pwd+=random.choice(chars)
+            pwd+=random.choice(digits)
+            if rank(pwd) == "STRONG":
+                break
+        return pwd
         # Hint: user random.choice to select a random Upperalphabet(Ualphabet), Lalphabet, chars, and digits. Join then all together in pwd and check ranking
         # While the required ranking is not met continue joining new Ualphabet, Lalphabet, chars and digits.
         
         ## START CODE HERE
-        password = ''.join(random.choices(string.ascii_letters + string.digits + string.punctuation, k =12))
-        return password
-        ## END CODE HERE
-        return pwd
-    
-    # Ask for username and check 20 character limits
+    username = input("Enter your username: ")
+    if len(username) > 20:
+        print("username length is more than 20, please re enter it")
+        option2()
+    else:
+        pass
+    password = gen_pass()
+    print('#'*80)
+    print('[Username]: '+ username)
+    print('[Password]: '+ password)
+    print('#'*80)
 
-    ## START CODE HERE
+    print('Do you want to save the password?')
+    answer = input("please return Y to save and N to not save: ").upper()
 
-    ## END CODE HERE
+    if answer == 'Y':
+        print('Do you want to save the password in your custom file location?')
+        ques = input("please return Y  or N: ").upper()
+        if ques == "Y":
+            path = input('Enter your absolute path here: ') 
+            if path[-3:].upper() == 'TXT':
+                try:
+                    f = open(path,'a')
+                    write_file(f, (username,password))
+                    f.close()
+                except Exception as e:
+                    print('\n'+'*'*80)
+                    print('Exception:' + str(e))
+                    print("Invalid file path, please enter a valid file path")
+                    print('*'*80+'\n')
+            else:
+                try:
+                    path = path+'.txt'
+                    f = open(path,'a')
+                    write_file(f, (username,password))
+                    f.close()
+                except Exception as e:
+                    print('\n'+'*'*80)
+                    print('Exception:' + str(e))
+                    print("Invalid file path, please enter a valid file path")
+                    print('*'*80+'\n')
 
-    # Generate the password using generate() and follow the steps as guided in the function header. 
+        else:
+            # write to the file as suggested in the question prompt
+            print('-'*10 + 'SAVING USERNAME AND PASSWORD in Users-Pwds.txt' + '-' * 10)
+            f = open(os.path.join(dirname,'Users-Pwds.txt'),'a')
+            write_file(f,(username,password))
+            f.close()
+            
+    elif answer == 'N':
+        print('-'*10 + 'Would you like to generate a new password?' + '-' * 10)
+        answer = input("please return Y or N : ").upper()
+        if answer == "Y":
+            option2()
+        else:
+            print('-'*10 + 'Restarting the program...' + '-' * 10)
 
-    ## START CODE HERE
+            pass 
+    else: 
+        print('Either choose Y or N, restarting the program...')
 
-    ## END CODE HERE
+def check_int_type(inp):
+    try:
+        inp = int(inp)
+        return inp
+    except Exception as exception:
+        print('\n'+'*'*80)
+        print('Exception:' + str(exception))
+        print("Invalid option, please only enter the number of option and omit other details. Restarting the program....")
+        print('#'*80+'\n')    
 
 def main():
 
@@ -164,6 +301,16 @@ def main():
         # exit the loop by using the break command if the user selects 3 other wise use option1() and option 2() function 
 
         ## START CODE HERE
+        if inp == 1:
+            option1()
+        elif inp == 2:
+            option2()
+        elif inp ==3:
+            print('-'*45)
+            print('Closing the program \nThis program was built by: Ashim Dahal')
+            break
+        else:
+            print("Please choose a number between 1 and 3.")
 
         ## END CODE HERE
 
